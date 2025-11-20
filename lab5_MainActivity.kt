be354411daddf008206dc115feb0740f1644abb5
bnnmuu <161942433+bnnmuu@users.noreply.github.com>
2025-11-20 21:06:04 +0800

    Refactor Kotlin inheritance and property access syntax

--- lab5_MainActivity.kt
+++ lab5_MainActivity.kt
@@ -8,9 +8,9 @@ import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
 import androidx.viewpager2.widget.ViewPager2
 
-// [Java 差異]: 繼承寫法
-// Java: public class MainActivity extends AppCompatActivity { ... }
-// Kotlin: 使用冒號 (:) 表示繼承，語法更乾淨。
+// [Java 差異]: 繼承語法
+// Java: public class MainActivity extends AppCompatActivity
+// Kotlin: 使用 : AppCompatActivity()，簡潔明瞭。
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -26,18 +26,18 @@ class MainActivity : AppCompatActivity() {
         Log.e("MainActivity", "onCreate")
         
         // [Java 差異]: 泛型推斷
-        // Java: ViewPager2 viewPager2 = (ViewPager2) findViewById(R.id.viewPager2); 需要轉型。
-        // Kotlin: findViewById<ViewPager2>(...) 利用泛型直接指定型別，無需強制轉型。
+        // Java: ViewPager2 vp = (ViewPager2) findViewById(...); 需要強制轉型。
+        // Kotlin: findViewById<ViewPager2>(...) 使用泛型，更安全且不需要轉型。
         val viewPager2 = findViewById<ViewPager2>(R.id.viewPager2)
         
         val adapter = ViewPagerAdapter(supportFragmentManager, this.lifecycle)
         
-        // [優化程式碼心得]: 屬性存取語法 (Property Access Syntax)
+        // [優化程式碼心得]: 屬性存取 (Property Access Syntax)
         // Java: viewPager2.setAdapter(adapter); 必須呼叫 Setter 方法。
-        // Kotlin: viewPager2.adapter = adapter 直接像存取變數一樣賦值，背後自動呼叫 setter，讀起來更直觀。
+        // Kotlin: viewPager2.adapter = adapter 直接賦值，語法像存取變數一樣自然，背後自動呼叫 setter。
         viewPager2.adapter = adapter
         
-        // 同樣原理，Java 需寫 setOffscreenPageLimit(1)
+        // 同理，取代 setOffscreenPageLimit(1)
         viewPager2.offscreenPageLimit = 1
     }
 
